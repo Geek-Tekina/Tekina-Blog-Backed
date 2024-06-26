df58c4988274f83ed5531dba291a1aa996907d0e
Geek-Tekina <tpbc1717@gmail.com>
2024-06-26 11:38:58 +0530

    initial db

---
+++ routes/userRouter.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  register,
+  login,
+  logout,
+  getMyProfile,
+  getAllAuthors,
+} from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+router.get("/myprofile", isAuthenticated, getMyProfile);
+router.get("/authors", getAllAuthors);
+
+export default router;
